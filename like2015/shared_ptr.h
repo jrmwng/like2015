3c8a7e9e609e49960bc80abf6435ac7420a9d40e
Wong Wang Leong <wwangleon2@mslive.cityu.edu.hk>
2015-09-19 21:30:07 +0800

    make_shared, test for compilation, remove intrinc, remove volatile, copy constructors, move constructors, aliasing constructors

--- like2015/shared_ptr.h
+++ like2015/shared_ptr.h
@@ -12,26 +12,32 @@ namespace like2015
 
 	struct shared_ptr_count
 	{
+		typedef long count_type;
+		std::atomic<count_type> lRefCount;
+
 		enum
 		{
 			SHARED_PTR_COUNT_BASE = 1,
 			SHARED_PTR_COUNT_THIS = SHARED_PTR_COUNT_BASE,
-			SHARED_PTR_COUNT_THAT_SHIFT = 16,
+			SHARED_PTR_COUNT_THAT_SHIFT = sizeof(count_type) * 4,
 			SHARED_PTR_COUNT_THAT = (1 << SHARED_PTR_COUNT_THAT_SHIFT),
 			SHARED_PTR_COUNT_BOTH = SHARED_PTR_COUNT_THIS + SHARED_PTR_COUNT_THAT,
 			SHARED_PTR_COUNT_INIT = SHARED_PTR_COUNT_BASE + SHARED_PTR_COUNT_BOTH,
 			SHARED_PTR_COUNT_MASK = 0xFFFF,
 		};
 
-		typedef long count_type;
-		std::atomic<count_type> lRefCount;
-
 		shared_ptr_count(void)
 			: lRefCount(SHARED_PTR_COUNT_INIT)
 		{}
+		virtual ~shared_ptr_count(void)
+		{}
 
-		virtual void delete_this(void) = 0;
-		virtual void delete_that(void) = 0;
+		void delete_this(void)
+		{
+			delete this;
+		}
+		virtual void delete_that(void)
+		{}
 
 		long use_count(void) const
 		{
@@ -82,7 +88,7 @@ namespace like2015
 			count_type lRefCount0;
 			count_type lRefCount1;
 			{
-				for (lRefCount0 = lRefCount; lRefCount0 >= SHARED_PTR_COUNT_THAT && !lRefCount.compare_exchange_weak(lRefCount1 = lRefCount0 + SHARED_PTR_COUNT_BOTH, lRefCount0); lRefCount0 = lRefCount)
+				for (lRefCount0 = lRefCount; lRefCount0 >= SHARED_PTR_COUNT_THAT && !lRefCount.compare_exchange_weak(lRefCount0, lRefCount1 = lRefCount0 + SHARED_PTR_COUNT_BOTH); lRefCount0 = lRefCount)
 				{
 					_mm_pause();
 				}
@@ -101,7 +107,7 @@ namespace like2015
 			count_type lRefCount0;
 			count_type lRefCount1;
 			{
-				for (lRefCount0 = lRefCount; lRefCount0 >= SHARED_PTR_COUNT_THAT && !lRefCount.compare_exchange_weak(lRefCount1 = lRefCount0 + SHARED_PTR_COUNT_BOTH - SHARED_PTR_COUNT_THIS, lRefCount0); lRefCount0 = lRefCount)
+				for (lRefCount0 = lRefCount; lRefCount0 >= SHARED_PTR_COUNT_THAT && !lRefCount.compare_exchange_weak(lRefCount0, lRefCount1 = lRefCount0 + SHARED_PTR_COUNT_BOTH - SHARED_PTR_COUNT_THIS); lRefCount0 = lRefCount)
 				{
 					_mm_pause();
 				}
@@ -115,7 +121,7 @@ namespace like2015
 			}
 		}
 	};
-	template <typename T, typename TDeleter>
+	template <typename T, typename TDeleter = void>
 	struct shared_ptr_count_t
 		: shared_ptr_count
 	{
@@ -125,10 +131,8 @@ namespace like2015
 			: m_pt(pt)
 			, m_tDeleter(std::forward<TDeleter>(tDeleter))
 		{}
-		virtual void delete_this(void)
-		{
-			delete this;
-		}
+		virtual ~shared_ptr_count_t(void)
+		{}
 		virtual void delete_that(void)
 		{
 			m_tDeleter(m_pt);
@@ -143,19 +147,32 @@ namespace like2015
 		shared_ptr_count_t(T *pt)
 			: m_pt(pt)
 		{}
+		virtual ~shared_ptr_count_t(void)
+		{}
 
-		virtual void delete_this(void)
-		{
-			delete this;
-		}
 		virtual void delete_that(void)
 		{
 			delete m_pt;
 		}
 	};
 	template <typename T>
+	struct make_shared_ptr_count_t
+		: shared_ptr_count
+	{
+		T m_t;
+		template<class... TArgs>
+		make_shared_ptr_count_t(TArgs &&... _Args)
+			: m_t(std::forward<TArgs>(_Args)...)
+		{}
+		virtual ~make_shared_ptr_count_t(void)
+		{}
+		T * get(void) { return std::addressof(m_t); }
+	};
+	template <typename T>
 	class shared_ptr_base
 	{
+		friend class shared_ptr<T>;
+		friend class weak_ptr<T>;
 	protected:
 		T *m_pt;
 		shared_ptr_count *m_pCount;
@@ -165,13 +182,10 @@ namespace like2015
 			, m_pCount(pCount)
 		{}
 
-		T * get(void) const { return m_pt; }
-		operator bool(void) const { return m_pt != nullptr; }
-
 		long use_count(void) const
 		{
 			if (m_pCount != nullptr)
-				return (m_pCount->use_count();
+				return (m_pCount->use_count());
 			else
 				return 0;
 		}
@@ -189,8 +203,9 @@ namespace like2015
 	{
 		typedef shared_ptr_base<T> base_type;
 	protected:
-		weak_ptr_base(T *pt, shared_ptr_count *pCount)
-			: base_type(pt, pCount)
+		template<class... TArgs>
+		weak_ptr_base(TArgs &&... _Args)
+			: base_type(std::forward<TArgs>(_Args)...)
 		{}
 	};
 
@@ -204,6 +219,22 @@ namespace like2015
 		shared_ptr(void)
 			: base_type(nullptr, nullptr)
 		{}
+		shared_ptr(make_shared_ptr_count_t<T> *p)
+			: base_type(p != nullptr ? p->get() : nullptr, p)
+		{}
+		shared_ptr(T *pt)
+			: base_type(pt, new shared_ptr_count_t<T>(pt))
+		{}
+		template <typename TDeleter>
+		shared_ptr(T *pt, TDeleter && tDeleter)
+			: base_type(pt, new shared_ptr_count_t<T, TDeleter>(pt, std::forward<TDeleter>(tDeleter)))
+		{}
+		shared_ptr(shared_ptr<T> const & spThat)
+			: base_type(spThat.m_pt, spThat.m_pCount)
+		{
+			if (m_pCount != nullptr)
+				m_pCount->acquire_shared();
+		}
 		template <typename U>
 		shared_ptr(shared_ptr<U> const & spThat)
 			: base_type(spThat.m_pt, spThat.m_pCount)
@@ -232,13 +263,6 @@ namespace like2015
 			spThat.m_pt = nullptr;
 			spThat.m_pCount = nullptr;
 		}
-		shared_ptr(T *pt)
-			: base_type(pt, new shared_ptr_count_t<T>(pt))
-		{}
-		template <typename TDeleter>
-		shared_ptr(T *pt, TDeleter && tDeleter)
-			: base_type(pt, new shared_ptr_count_t<T, TDeleter>(pt, std::forward<TDeleter>(tDeleter)))
-		{}
 		template <typename U>
 		shared_ptr(weak_ptr_base<U> const & wpThat)
 			: base_type(wpThat.m_pt, wpThat.m_pCount)
@@ -295,14 +319,19 @@ namespace like2015
 				m_pCount->release_shared();
 		}
 
+		T * operator -> (void) const { return m_pt; }
+		T & operator * (void) const { return *m_pt; }
+
+		T * get(void) const { return m_pt; }
+		operator bool(void) const { return m_pt != nullptr; }
+
 		void swap(this_type & that)
 		{
-			std::swap(m_pCount, that.m_pCount);
-			_mm_sfence();
 			std::swap(m_pt, that.m_pt);
+			std::swap(m_pCount, that.m_pCount);
 		}
 		template<class... TArgs>
-		void reset(TArgs &&... _Args) volatile
+		void reset(TArgs &&... _Args)
 		{
 			swap(this_type(std::forward<TArgs>(_Args)...));
 		}
@@ -314,6 +343,40 @@ namespace like2015
 		typedef weak_ptr_base<T> base_type;
 		typedef weak_ptr<T> this_type;
 	public:
+		weak_ptr(weak_ptr<T> const & wpThat)
+			: base_type(wpThat.m_pt, wpThat.m_pCount)
+		{
+			if (m_pCount != nullptr)
+				m_pCount->acquire_weak();
+		}
+		template <typename U>
+		weak_ptr(weak_ptr<U> const & wpThat)
+			: base_type(wpThat.m_pt, wpThat.m_pCount)
+		{
+			if (m_pCount != nullptr)
+				m_pCount->acquire_weak();
+		}
+		template <typename U>
+		weak_ptr(weak_ptr<U> && wpThat)
+			: base_type(wpThat.m_pt, wpThat.m_pCount)
+		{
+			wpThat.m_pt = nullptr;
+			wpThat.m_pCount = nullptr;
+		}
+		template <typename U>
+		weak_ptr(weak_ptr<U> const & wpThat, T *pt)
+			: base_type(pt, wpThat.m_pCount)
+		{
+			if (m_pCount != nullptr)
+				m_pCount->acquire_weak();
+		}
+		template <typename U>
+		weak_ptr(weak_ptr<U> && wpThat, T *pt)
+			: base_type(pt, wpThat.m_pCount)
+		{
+			wpThat.m_pt = nullptr;
+			wpThat.m_pCount = nullptr;
+		}
 		template <typename U>
 		weak_ptr(shared_ptr<U> const & spThat)
 			: base_type(spThat.m_pt, spThat.m_pCount)
@@ -364,14 +427,18 @@ namespace like2015
 
 		void swap(this_type & that)
 		{
-			std::swap(m_pCount, that.m_pCount);
-			_mm_sfence();
 			std::swap(m_pt, that.m_pt);
+			std::swap(m_pCount, that.m_pCount);
 		}
 		template<class... TArgs>
-		void reset(TArgs &&... _Args) volatile
+		void reset(TArgs &&... _Args)
 		{
 			swap(this_type(std::forward<TArgs>(_Args)...));
 		}
 	};
+	template<typename T, class... TArgs>
+	shared_ptr<T> make_shared(TArgs &&... _Args)
+	{
+		return std::move(shared_ptr<T>(new make_shared_ptr_count_t<T>(std::forward<TArgs>(_Args)...)));
+	}
 }
