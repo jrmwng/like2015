5a4d7bac9e86e5b5c13938240a71b8edbba428e5
Wong Wang Leong <wwangleon2@mslive.cityu.edu.hk>
2015-09-21 01:14:31 +0800

    namespace like, include stdint.h

--- like2015/shared_lock.h
+++ like2015/shared_lock.h
@@ -1,45 +1,49 @@
 #pragma once
 
 #include <intrin.h>
+#include <stdint.h>
 #include <algorithm>
 #include <numeric>
 #include <functional>
 
-template <std::uint8_t uCount16>
-struct __declspec(align(16)) shared_lock
+namespace like
 {
-	__m128i axmm[uCount16];
+	template <uint8_t uCount16>
+	struct __declspec(align(16)) shared_lock
+	{
+		__m128i axmm[uCount16];
 
-	shared_lock(void)
-	{
-		std::fill(std::begin(axmm), std::end(axmm), _mm_setzero_si128());
-	}
-
-	void read_begin(std::uint8_t uIndex)
-	{
-		axmm->m128i_u8[uIndex] = 0x80; // TODO: atomic semantics
-	}
-	void read_end(std::uint8_t uIndex)
-	{
-		axmm->m128i_u8[uIndex] = 0; // TODO: atomic semantics
-	}
-	void read_sync(void)
-	{
-		int an[uCount16];
+		shared_lock(void)
 		{
-			std::transform(std::cbegin(axmm), std::cend(axmm), an, [](__m128i const & xmm)->int
-			{
-				return _mm_movemask_epi8(xmm);
-			});
+			std::fill(std::begin(axmm), std::end(axmm), _mm_setzero_si128());
 		}
-		while (std::accumulate(std::cbegin(an), std::cend(an), 0, std::bit_or<int>()))
-		{
-			_mm_pause();
 
-			std::transform(std::cbegin(axmm), std::cend(axmm), an, an, [](__m128i const & xmm, int n)->int
-			{
-				return _mm_movemask_epi8(xmm) & n;
-			});
+		void read_begin(uint8_t uIndex)
+		{
+			axmm->m128i_u8[uIndex] = 0x80; // TODO: atomic semantics
 		}
-	}
-};
+		void read_end(uint8_t uIndex)
+		{
+			axmm->m128i_u8[uIndex] = 0; // TODO: atomic semantics
+		}
+		void read_sync(void)
+		{
+			int an[uCount16];
+			{
+				std::transform(std::cbegin(axmm), std::cend(axmm), an, [](__m128i const & xmm)->int
+				{
+					return _mm_movemask_epi8(xmm);
+				});
+			}
+			while (std::accumulate(std::cbegin(an), std::cend(an), 0, std::bit_or<int>()))
+			{
+				_mm_pause();
+
+				std::transform(std::cbegin(axmm), std::cend(axmm), an, an, [](__m128i const & xmm, int n)->int
+				{
+					return _mm_movemask_epi8(xmm) & n;
+				});
+			}
+		}
+	};
+}
