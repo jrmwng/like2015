a973e4bd0555a8309022a28d0ceaaa4e92a8b5d4
Jeremy Wong <jmw@netvigator.com>
2015-12-13 20:51:50 +0800

    result_type

--- like2015/functional.h
+++ like2015/functional.h
@@ -13,17 +13,150 @@ namespace like
 	template <typename TR>
 	struct member_t<TR>
 	{
-		typedef TR type;
+		//template <typename T1>
+		//typename std::enable_if<std::is_void<TR>::value, T1 &>::type operator () (T1 & t1) const
+		//{
+		//	return t1;
+		//}
+		//template <typename T1>
+		//typename std::enable_if<!std::is_void<TR>::value, TR>::type operator () (T1 & t1) const
+		//{
+		//	return t1;
+		//}
+	};
+
+	template <typename TR, typename TS, typename TV>
+	struct member_t<TR, TV(TS::*)>
+		: member_t<TR>
+	{
+		typedef member_t<TR> base_type;
+		typedef typename std::conditional<std::is_void<TR>::value, TV, TR>::type result_type;
+
+		typedef TV(TS::*TM);
+
+		TM const m_tM;
+
+		member_t(TM const & tM)
+			: m_tM(tM)
+		{}
 
 		template <typename T1>
-		typename std::enable_if<(!std::is_void<TR>::value), TR>::type operator () (T1 & t1) const
+		decltype(auto) operator () (T1 & t1) const
 		{
-			return t1;
+			return (t1.*m_tM);
 		}
 		template <typename T1>
-		typename std::enable_if<(std::is_void<TR>::value), T1 &>::type operator () (T1 & t1) const
+		decltype(auto) operator () (T1 *pt1) const
 		{
-			return t1;
+			return (pt1->*m_tM);
+		}
+	};
+
+	template <typename TR, typename TS, typename TV>
+	struct member_t<TR, TV(TS::*)()>
+		: member_t<TR>
+	{
+		typedef member_t<TR> base_type;
+		typedef typename std::conditional<std::is_void<TR>::value, TV, TR>::type result_type;
+
+		typedef TV(TS::*TM)();
+
+		TM const m_tM;
+
+		member_t(TM const & tM)
+			: m_tM(tM)
+		{}
+
+		template <typename T1>
+		decltype(auto) operator () (T1 & t1) const
+		{
+			return (t1.*m_tM)();
+		}
+		template <typename T1>
+		decltype(auto) operator () (T1 *pt1) const
+		{
+			return (pt1->*m_tM)();
+		}
+	};
+
+	template <typename TR, typename TS, typename TV>
+	struct member_t<TR, TV(TS::*)()const>
+		: member_t<TR>
+	{
+		typedef member_t<TR> base_type;
+		typedef typename std::conditional<std::is_void<TR>::value, TV, TR>::type result_type;
+
+		typedef TV(TS::*TM)()const;
+
+		TM const m_tM;
+
+		member_t(TM const & tM)
+			: m_tM(tM)
+		{}
+
+		template <typename T1>
+		decltype(auto) operator () (T1 & t1) const
+		{
+			return (t1.*m_tM)();
+		}
+		template <typename T1>
+		decltype(auto) operator () (T1 *pt1) const
+		{
+			return (pt1->*m_tM)();
+		}
+	};
+
+	template <typename TR, typename TS, typename TV>
+	struct member_t<TR, TV(TS::*)()volatile>
+		: member_t<TR>
+	{
+		typedef member_t<TR> base_type;
+		typedef typename std::conditional<std::is_void<TR>::value, TV, TR>::type result_type;
+
+		typedef TV(TS::*TM)()volatile;
+
+		TM const m_tM;
+
+		member_t(TM const & tM)
+			: m_tM(tM)
+		{}
+
+		template <typename T1>
+		decltype(auto) operator () (T1 & t1) const
+		{
+			return (t1.*m_tM)();
+		}
+		template <typename T1>
+		decltype(auto) operator () (T1 *pt1) const
+		{
+			return (pt1->*m_tM)();
+		}
+	};
+
+	template <typename TR, typename TS, typename TV>
+	struct member_t<TR, TV(TS::*)()const volatile>
+		: member_t<TR>
+	{
+		typedef member_t<TR> base_type;
+		typedef typename std::conditional<std::is_void<TR>::value, TV, TR>::type result_type;
+
+		typedef TV(TS::*TM)()const volatile;
+
+		TM const m_tM;
+
+		member_t(TM const & tM)
+			: m_tM(tM)
+		{}
+
+		template <typename T1>
+		decltype(auto) operator () (T1 & t1) const
+		{
+			return (t1.*m_tM)();
+		}
+		template <typename T1>
+		decltype(auto) operator () (T1 *pt1) const
+		{
+			return (pt1->*m_tM)();
 		}
 	};
 
@@ -102,17 +235,17 @@ namespace like
 			, m_tOrder(tOrder)
 		{}
 
-		bool operator () (typename TM::type const & t1, typename TM::type const & t2) const
+		bool operator () (typename TM::result_type const & t1, typename TM::result_type const & t2) const
 		{
 			return m_tOrder(t1, t2);
 		}
 		template <typename T1>
-		bool operator () (T1 const & t1, typename TM::type const & t2) const
+		bool operator () (T1 const & t1, typename TM::result_type const & t2) const
 		{
 			return m_tOrder(m_tM(t1), t2);
 		}
 		template <typename T2>
-		bool operator () (typename TM::type const & t1, T2 const & t2) const
+		bool operator () (typename TM::result_type const & t1, T2 const & t2) const
 		{
 			return m_tOrder(t1, m_tM(t2));
 		}
@@ -123,12 +256,12 @@ namespace like
 		}
 	};
 
-	template <typename TM, typename TOrder = std::less<typename TM::type>>
+	template <typename TM, typename TOrder = std::less<typename TM::result_type>>
 	order_by_t<TM, TOrder> order_by(TM const & tM, TOrder const & tOrder = TOrder())
 	{
 		return order_by_t<TM, TOrder>(tM, tOrder);
 	}
-	template <typename TS, typename TV, typename TM = member_t<TV, TV(TS::*)>, typename TOrder = std::less<typename TM::type>>
+	template <typename TS, typename TV, typename TM = member_t<TV, TV(TS::*)>, typename TOrder = std::less<typename TM::result_type>>
 	order_by_t<TM, TOrder> order_by(TV(TS::*pM), TOrder const & tOrder = TOrder())
 	{
 		return order_by_t<TM, TOrder>(TM(pM), tOrder);
