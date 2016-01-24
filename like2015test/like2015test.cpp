0194adf2a5e1b4de8b979e14c7a9e2f1cb692c2a
jrmwng <jmw@netvigator.com>
2016-01-24 23:07:11 +0800

    simplify by cpuid_tree, test for tsx

--- like2015test/like2015test.cpp
+++ like2015test/like2015test.cpp
@@ -52,56 +52,16 @@ struct SMain
 
 int main(int nArgc)
 {
-	like::cpuid_t<0x00> cpuid00;
-	__m128i xmmVendorID = cpuid00.vendor_identification_string();
-	like::cpuid_t<0x01> cpuid01;
-	like::cpuid_t<0x02> cpuid02;
-	like::cpuid_t<0x04, 0> cpuid04_0;
-	like::cpuid_t<0x04, 1> cpuid04_1;
-	like::cpuid_t<0x04, 2> cpuid04_2;
-	like::cpuid_t<0x04, 3> cpuid04_3;
-	like::cpuid_t<0x04, 4> cpuid04_4;
-	like::cpuid_t<0x05> cpuid05;
-	like::cpuid_t<0x06> cpuid06;
-	like::cpuid_t<0x07> cpuid07;
-	like::cpuid_t<0x09> cpuid09;
-	like::cpuid_t<0x0A> cpuid0A;
-	like::cpuid_t<0x0B, 0> cpuid0B0;
-	like::cpuid_t<0x0B, 1> cpuid0B1;
-	like::cpuid_t<0x0B, 2> cpuid0B2;
-	like::cpuid_t<0x0C> cpuid0C;
-	like::cpuid_t<0x0D> cpuid0D;
-	like::cpuid_t<0x0D, 1> cpuid0D1;
-	like::cpuid_t<0x0D, 2> cpuid0D2_AVX;
-	like::cpuid_t<0x0D, 3> cpuid0D3_BNDREG;
-	like::cpuid_t<0x0D, 4> cpuid0D4_BNDCSR;
-	like::cpuid_t<0x0D, 5> cpuid0D5_Opmask;
-	like::cpuid_t<0x0D, 6> cpuid0D6_ZMM_Hi256;
-	like::cpuid_t<0x0D, 7> cpuid0D7_Hi16_ZMM;
-	like::cpuid_t<0x0D, 8> cpuid0D8_PT;
-	like::cpuid_t<0x0D, 9> cpuid0D9_PKRU;
-	like::cpuid_t<0x0F> cpuid0F;
-	like::cpuid_t<0x0F, 1> cpuid0F1;
-	like::cpuid_t<0x10> cpuid10;
-	like::cpuid_t<0x10, 1> cpuid101;
-	like::cpuid_t<0x14> cpuid14;
-	like::cpuid_t<0x14, 1> cpuid141;
-	like::cpuid_t<0x15> cpuid15;
-	like::cpuid_t<0x16> cpuid16;
-	like::cpuid_t<0x17> cpuid17;
-	like::cpuid_soc_vendor_brand_string_t cpuidSocVendorBrandString;
-	like::cpuid_t<0x80000000> cpuid80000000;
-	like::cpuid_t<0x80000001> cpuid80000001;
-	like::cpuid_t<0x80000002> cpuid80000002;
-	like::cpuid_t<0x80000003> cpuid80000003;
-	like::cpuid_t<0x80000004> cpuid80000004;
-	like::cpuid_processor_brand_string_t cpuidProcessorBrandString;
-	like::cpuid_t<0x80000006> cpuid80000006;
-	like::cpuid_t<0x80000007> cpuid80000007;
-	like::cpuid_t<0x80000008> cpuid80000008;
+	like::cpuid_tree_t<0x17> stCPUID;
+	like::cpuid_tree_t<0x80000008> stExtendedCPUID;
 
-	char const *pcSocVendorBrandString = cpuidSocVendorBrandString;
-	char const *pcProcessorBrandString = cpuidProcessorBrandString;
+	__m128i const xmmVendorIdentString = stCPUID.vendor_identification_string();
+	char const *pcSocVendorBrandString = stCPUID.soc_vendor_brand_string();
+	char const *pcProcessorBrandString = stExtendedCPUID.processor_brand_string();
+
+	std::cout << xmmVendorIdentString.m128i_i8 << std::endl;
+	std::cout << pcSocVendorBrandString << std::endl;
+	std::cout << pcProcessorBrandString << std::endl;
 
 	std::cout <<
 		std::setw(8) << "EAX" << std::ends <<
@@ -110,34 +70,8 @@ int main(int nArgc)
 		std::setw(8) << "EBX" << std::ends <<
 		std::setw(8) << "ECX" << std::ends <<
 		std::setw(8) << "EDX" << std::endl <<
-		cpuid00 <<
-		cpuid01 <<
-		cpuid04_0 <<
-		cpuid04_1 <<
-		cpuid04_2 <<
-		cpuid04_3 <<
-		cpuid04_4 <<
-		cpuid05 <<
-		cpuid06 <<
-		cpuid07 <<
-		cpuid09 <<
-		cpuid0A <<
-		cpuid0B0 <<
-		cpuid0B1 <<
-		cpuid0B2 <<
-		cpuid0C <<
-		cpuid0D <<
-		cpuid0D1 <<
-		cpuid0D2_AVX <<
-		cpuid80000000 <<
-		cpuid80000001 <<
-		cpuid80000002 <<
-		cpuid80000003 <<
-		cpuid80000004 <<
-		cpuid80000006 <<
-		cpuid80000007 <<
-		cpuid80000008
-		;
+		stCPUID <<
+		stExtendedCPUID;
 
  	like::xmm_int32<1> xmmInt1(1);
 	like::xmm_int32<2> xmmInt2(1, 2);
@@ -332,7 +266,10 @@ int main(int nArgc)
 #if 1
 			like_shared_ptr<int> sp(std::move(spX.load()));
 
-			nRET += n + *sp;
+			for (int i = 0; i < n; i++)
+				nRET += i + *sp;
+
+			//_mm_pause();
 #else
 			like_weak_ptr<int> wp(sp4);
 			like_shared_ptr<int> sp(std::move(wp));
@@ -343,11 +280,12 @@ int main(int nArgc)
 #endif
 		};
 
+		int const N = 1000;
 		unsigned long long uxl0 = __rdtsc();
-		for (int n = 0; n < 1000000; n++)
+		for (int n = 0; n < N; n++)
 			_Test(n);
 		unsigned long long uxl1 = __rdtsc();
-		concurrency::parallel_for(1, 1000000, _Test, concurrency::static_partitioner());
+		concurrency::parallel_for(1, N, _Test, concurrency::static_partitioner());
 		unsigned long long uxl2 = __rdtsc();
 
 		printf("%I64u\n", uxl1 - uxl0);
