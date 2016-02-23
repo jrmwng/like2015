52ff831d32b3ab7b5992d359e6a24ce7a6c4c2f5
jrmwng <jmw@netvigator.com>
2016-02-23 21:17:47 +0800

    add project UnitTest_jrmwng

---
+++ UnitTest_jrmwng/UnitTest_jrmwng.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest_jrmwng
+{		
+	TEST_CLASS(UnitTest_jrmwng)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod_jrmwng)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
