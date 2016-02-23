eb0a7426eb4a1c601f591edcb9eed53d1cc140d6
jrmwng <jmw@netvigator.com>
2016-02-23 22:29:26 +0800

    codes for TestMethod_jrmwng_align

--- UnitTest_jrmwng/UnitTest_jrmwng.cpp
+++ UnitTest_jrmwng/UnitTest_jrmwng.cpp
@@ -9,9 +9,17 @@ namespace UnitTest_jrmwng
 	{
 	public:
 		
-		TEST_METHOD(TestMethod_jrmwng)
+		TEST_METHOD(TestMethod_jrmwng_align)
 		{
-			// TODO: Your test code here
+			auto const stAlignPage = jrmwng::align_page(0);
+			auto const stAlignSect = jrmwng::align_section(0);
+			auto const stAlignLine = jrmwng::align_line(0);
+			auto const stAlignLock = jrmwng::align_lock(0);
+
+			auto const uAlignPage = reinterpret_cast<uintptr_t>(&stAlignPage);
+			auto const uAlignSect = reinterpret_cast<uintptr_t>(&stAlignSect);
+			auto const uAlignLine = reinterpret_cast<uintptr_t>(&stAlignLine);
+			auto const uAlignLock = reinterpret_cast<uintptr_t>(&stAlignLock);
 		}
 
 	};
