00ee4ad22bb850a058af1114e1002842fa3e33de
jrmwng <jmw@netvigator.com>
2016-05-23 08:26:17 +0800

    assign_sub

--- jrmwng/big_number_mm.h
+++ jrmwng/big_number_mm.h
@@ -23,25 +23,25 @@ namespace jrmwng
 		bnThat.xmmValue = xmmValue;
 	}
 	template <unsigned uIndex, unsigned uThat>
-	static std::enable_if_t<(big_number<__m128i, uThat>::INDEX < uIndex)> get_value(big_number<__m128i, uThat> const & bnThat, __m128i const & xmmValue)
+	static std::enable_if_t<(big_number<__m128i, uThat>::INDEX < uIndex)> set_value(big_number<__m128i, uThat> const & bnThat, __m128i const & xmmValue)
 	{
 		if (0xFFFF != _mm_movemask_epi8(_mm_cmpeq_epi32(xmmValue, _mm_setzero_si128())))
 		{
 			__debugbreak();
 		}
 	}
-	template <unsigned uIndex, unsigned uThat, unsigned uLeft, unsigned uRight>
+	template <unsigned uIndex, int nXor, unsigned uThat, unsigned uLeft, unsigned uRight>
 	static std::enable_if_t<(0 == uIndex), unsigned char> assign_add(big_number<__m128i, uThat> & bnThat, big_number<__m128i, uLeft> const & bnLeft, big_number<__m128i, uRight> const & bnRight)
 	{
 		return 0;
 	}
-	template <unsigned uIndex, unsigned uThat, unsigned uLeft, unsigned uRight>
+	template <unsigned uIndex, int nXor, unsigned uThat, unsigned uLeft, unsigned uRight>
 	static std::enable_if_t<(0 < uIndex), unsigned char> assign_add(big_number<__m128i, uThat> & bnThat, big_number<__m128i, uLeft> const & bnLeft, big_number<__m128i, uRight> const & bnRight)
 	{
-		__m128i const u4Left = get_value<uIndex>(bnLeft);
-		__m128i const u4Right = get_value<uIndex>(bnRight);
+		unsigned char const ubCarryBase = assign_add<uIndex - 1, nXor>(bnThat, bnLeft, bnRight);
 
-		unsigned char const ubCarryBase = assign_add<uIndex - 1>(bnThat, bnLeft, bnRight);
+		__m128i const u4Left = _mm_xor_si128(get_value<uIndex>(bnLeft), _mm_set1_epi32(nXor));
+		__m128i const u4Right = get_value<uIndex>(bnRight);
 		unsigned uLaddR0;
 		unsigned uLaddR1;
 		unsigned uLaddR2;
@@ -53,7 +53,7 @@ namespace jrmwng
 
 		__m128i const u4LaddR = _mm_set_epi32(uLaddR3, uLaddR2, uLaddR1, uLaddR0);
 
-		set_value<uIndex>(bnThat, u4LaddR);
+		set_value<uIndex>(bnThat, _mm_xor_si128(u4LaddR, _mm_set1_epi32(nXor));
 		return ubCarry3;
 	}
 	template <>
@@ -80,7 +80,17 @@ namespace jrmwng
 		{
 			auto const & bnLeft = std::get<0>(stAdd);
 			auto const & bnRight = std::get<1>(stAdd);
-			if (assign_add(*this, bnLeft, bnRight))
+			if (assign_add<(big_number<__m128i, std::max<unsigned>(uBitCount, std::max<unsigned>(uLeft, uRight))>::INDEX, 0>(*this, bnLeft, bnRight))
+			{
+				__debugbreak();
+			}
+		}
+		template <unsigned uLeft, unsigned uRight>
+		big_number(big_number_sub<big_number<__m128i, uLeft> const &, big_number<__m128i, uRight> const &> stSub)
+		{
+			auto const & bnLeft = std::get<0>(stAdd);
+			auto const & bnRight = std::get<1>(stAdd);
+			if (assign_add<(big_number<__m128i, std::max<unsigned>(uBitCount, std::max<unsigned>(uLeft, uRight))>::INDEX, ~0>(*this, bnLeft, bnRight))
 			{
 				__debugbreak();
 			}
