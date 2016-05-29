02684085b927d5681e4601698405800ad65e1d61
jrmwng <jmw@netvigator.com>
2016-05-29 23:37:00 +0800

    add constexpr to function

--- jrmwng/tuple.h
+++ jrmwng/tuple.h
@@ -9,23 +9,23 @@ namespace jrmwng
 	namespace internals
 	{
 		template <typename Ttuple, typename Tfunc, size_t... uIndex>
-		void for_each_tuple(Ttuple && tTuple, Tfunc && tFunc, std::index_sequence<uIndex...>)
+		constexpr void for_each_tuple(Ttuple && tTuple, Tfunc && tFunc, std::index_sequence<uIndex...>)
 		{
 			std::make_tuple((std::forward<Tfunc>(tFunc)(std::get<uIndex>(tTuple)), 0)...);
 		}
 	}
 	template <typename Ttuple, typename Tfunc>
-	void for_each_tuple(Ttuple && tTuple, Tfunc && tFunc)
+	constexpr void for_each_tuple(Ttuple && tTuple, Tfunc && tFunc)
 	{
 		internals::for_each_tuple(std::forward<Ttuple>(tTuple), std::forward<Tfunc>(tFunc), std::make_index_sequence<std::tuple_size<std::decay_t<Ttuple>>::value>());
 	}
 	template <typename Tfunc, typename... Ttuple>
-	void for_each(std::tuple<Ttuple...> && tTuple, Tfunc && tFunc)
+	constexpr void for_each(std::tuple<Ttuple...> && tTuple, Tfunc && tFunc)
 	{
 		internals::for_each_tuple(std::forward<std::tuple<Ttuple...>>(tTuple), std::forward<Tfunc>(tFunc), std::index_sequence_for<Ttuple...>());
 	}
 	template <typename Tfunc, typename... Ttuple>
-	void for_each(std::tuple<Ttuple...> const & tTuple, Tfunc && tFunc)
+	constexpr void for_each(std::tuple<Ttuple...> const & tTuple, Tfunc && tFunc)
 	{
 		internals::for_each_tuple(tTuple, std::forward<Tfunc>(tFunc), std::index_sequence_for<Ttuple...>());
 	}
@@ -33,18 +33,18 @@ namespace jrmwng
 	namespace internals
 	{
 		template <typename Ttuple, typename Tfunc, size_t... uIndex>
-		auto transform_tuple(Ttuple && tTuple, Tfunc && tFunc, std::index_sequence<uIndex...>)
+		constexpr auto transform_tuple(Ttuple && tTuple, Tfunc && tFunc, std::index_sequence<uIndex...>)
 		{
 			return std::make_tuple(std::forward<Tfunc>(tFunc)(std::get<uIndex>(tTuple))...);
 		}
 	}
 	template <typename Ttuple, typename Tfunc>
-	auto transform_tuple(Ttuple && tTuple, Tfunc && tFunc)
+	constexpr auto transform_tuple(Ttuple && tTuple, Tfunc && tFunc)
 	{
 		return internals::transform_tuple(std::forward<Ttuple>(tTuple), std::forward<Tfunc>(tFunc), std::make_index_sequence<std::tuple_size<std::decay_t<Ttuple>>::value>());
 	}
 	template <typename Tinput, typename Tfunc, typename... Toutput>
-	void transform(Tinput && tInput, std::tuple<Toutput...> & tOutput, Tfunc && tFunc)
+	constexpr void transform(Tinput && tInput, std::tuple<Toutput...> & tOutput, Tfunc && tFunc)
 	{
 		tOutput = internals::transform_tuple(std::forward<Tinput>(tInput), std::forward<Tfunc>(tFunc), std::index_sequence_for<Toutput...>());
 	}
@@ -52,23 +52,23 @@ namespace jrmwng
 	namespace internals
 	{
 		template <typename Tfunc, typename Ttuple, size_t... uIndex>
-		static auto apply_tuple(Tfunc && tFunc, Ttuple && tTuple, std::index_sequence<uIndex...>)
+		constexpr auto apply_tuple(Tfunc && tFunc, Ttuple && tTuple, std::index_sequence<uIndex...>)
 		{
 			return std::forward<Tfunc>(tFunc)(std::get<uIndex>(std::forward<Ttuple>(tTuple))...);
 		}
 	}
 	template <typename Tfunc, typename Ttuple>
-	auto apply_tuple(Tfunc && tFunc, Ttuple && tTuple)
+	constexpr auto apply_tuple(Tfunc && tFunc, Ttuple && tTuple)
 	{
 		return internals::apply_tuple(std::forward<Tfunc>(tFunc), std::forward<Ttuple>(tTuple), std::make_index_sequence<std::tuple_size<std::decay_t<Ttuple>>::value>());
 	}
 	template <typename Tfunc, typename... Ttuple>
-	auto apply(Tfunc && tFunc, std::tuple<Ttuple...> && tTuple)
+	constexpr auto apply(Tfunc && tFunc, std::tuple<Ttuple...> && tTuple)
 	{
 		return internals::apply_tuple(std::forward<Tfunc>(tFunc), std::forward<std::tuple<Ttuple...>>(tTuple), std::index_sequence_for<Ttuple...>());
 	}
 	template <typename Tfunc, typename... Ttuple>
-	auto apply(Tfunc && tFunc, std::tuple<Ttuple...> const & tTuple)
+	constexpr auto apply(Tfunc && tFunc, std::tuple<Ttuple...> const & tTuple)
 	{
 		return internals::apply_tuple(std::forward<Tfunc>(tFunc), tTuple, std::index_sequence_for<Ttuple...>());
 	}
