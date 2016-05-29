33658465876207a0a023c0602a38fb7f54509d7c
jrmwng <jmw@netvigator.com>
2016-05-29 12:45:03 +0800

    implement for_each(tuple, fn) with index_sequence

--- jrmwng/tuple.h
+++ jrmwng/tuple.h
@@ -8,54 +8,26 @@ namespace jrmwng
 {
 	namespace internals
 	{
-		template <size_t uStart, size_t uEnd, size_t uSize = uEnd - uStart>
-		struct for_each_tuple_s
-			: for_each_tuple_s<uStart, (uStart + uEnd) / 2>
-			, for_each_tuple_s<(uStart + uEnd) / 2, uEnd>
+		template <typename Ttuple, typename Tfunc, size_t... uIndex>
+		void for_each_tuple(Ttuple && tTuple, Tfunc && tFunc, std::index_sequence<uIndex...>)
 		{
-			template <typename TTuple, typename TFunc>
-			static void apply(TTuple const & stTuple, TFunc const & tFunc)
-			{
-				for_each_tuple_s<uStart, (uStart + uEnd) / 2>::apply(stTuple, tFunc);
-				for_each_tuple_s<(uStart + uEnd) / 2, uEnd>::apply(stTuple, tFunc);
-			}
-		};
-		template <size_t uStart, size_t uEnd>
-		struct for_each_tuple_s<uStart, uEnd, 2>
-		{
-			template <typename TTuple, typename TFunc>
-			static void apply(TTuple const & stTuple, TFunc const & tFunc)
-			{
-				tFunc(std::get<uStart + 0>(stTuple));
-				tFunc(std::get<uStart + 1>(stTuple));
-			}
-		};
-		template <size_t uStart, size_t uEnd>
-		struct for_each_tuple_s<uStart, uEnd, 1>
-		{
-			template <typename TTuple, typename TFunc>
-			static void apply(TTuple const & stTuple, TFunc const & tFunc)
-			{
-				tFunc(std::get<uStart>(stTuple));
-			}
-		};
-		template <size_t uStart, size_t uEnd>
-		struct for_each_tuple_s<uStart, uEnd, 0>
-		{
-			template <typename TTuple, typename TFunc>
-			static void apply(TTuple const & stTuple, TFunc const & tFunc)
-			{}
-		};
+			std::make_tuple((std::forward<Tfunc>(tFunc)(std::get<uIndex>(tTuple)), 0)...);
+		}
 	}
-	template <typename Ttuple, typename TFunc>
-	void for_each_tuple(Ttuple const & stTuple, TFunc const & tFunc)
+	template <typename Ttuple, typename Tfunc>
+	void for_each_tuple(Ttuple && tTuple, Tfunc && tFunc)
 	{
-		internals::for_each_tuple_s<0, std::tuple_size<Ttuple>::value>::apply(stTuple, tFunc);
+		internals::for_each_tuple(std::forward<Ttuple>(tTuple), std::forward<Tfunc>(tFunc), std::make_index_sequence<std::tuple_size<std::decay_t<Ttuple>>::value>());
 	}
-	template <typename TFunc, typename... Ttuple>
-	void for_each(std::tuple<Ttuple...> const & stTuple, TFunc const & tFunc)
+	template <typename Tfunc, typename... Ttuple>
+	void for_each(std::tuple<Ttuple...> && tTuple, Tfunc && tFunc)
 	{
-		for_each_tuple(stTuple, tFunc);
+		internals::for_each_tuple(std::forward<std::tuple<Ttuple...>>(tTuple), std::forward<Tfunc>(tFunc), std::index_sequence_for<Ttuple...>());
+	}
+	template <typename Tfunc, typename... Ttuple>
+	void for_each(std::tuple<Ttuple...> const & tTuple, Tfunc && tFunc)
+	{
+		internals::for_each_tuple(tTuple, std::forward<Tfunc>(tFunc), std::index_sequence_for<Ttuple...>());
 	}
 
 	namespace internals
@@ -93,6 +65,11 @@ namespace jrmwng
 	template <typename Tfunc, typename... Ttuple>
 	auto apply(Tfunc && tFunc, std::tuple<Ttuple...> && tTuple)
 	{
-		return internals::apply_tuple(std::forward<Tfunc>(tFunc), std::forward<Ttuple>(tTuple), std::index_sequence_for<Ttuple...>());
+		return internals::apply_tuple(std::forward<Tfunc>(tFunc), std::forward<std::tuple<Ttuple...>>(tTuple), std::index_sequence_for<Ttuple...>());
+	}
+	template <typename Tfunc, typename... Ttuple>
+	auto apply(Tfunc && tFunc, std::tuple<Ttuple...> const & tTuple)
+	{
+		return internals::apply_tuple(std::forward<Tfunc>(tFunc), tTuple, std::index_sequence_for<Ttuple...>());
 	}
 }
